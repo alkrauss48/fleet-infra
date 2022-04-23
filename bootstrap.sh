4012b9fc2b73422090c2d2f160c518e934309827
aaron <alkrauss48@gmail.com>
2022-04-22 22:24:58 -0500

    Add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+export ./.env
+
+flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --owner=$GITHUB_USER \
+  --repository=fleet-infra \
+  --branch=main \
+  --path=./clusters/my-cluster \
+  --read-write-key \
+  --personal
